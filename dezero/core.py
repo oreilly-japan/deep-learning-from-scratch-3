2737d376c41f6364b70f7b752a0c67e51394e14e
Koki Saitoh <koki0702@gmail.com>
2019-11-15 09:43:28 +0900

    reshapeとtransposeを追加

--- dezero/core.py
+++ dezero/core.py
@@ -118,6 +118,19 @@ class Variable:
                 for y in f.outputs:
                     y().grad = None  # y is weakref
 
+    def reshape(self, *shape):
+        if len(shape) == 1 and isinstance(shape[0], (tuple, list)):
+            shape = shape[0]
+        return dezero.functions.reshape(self, shape)
+
+    def transpose(self, *axes):
+        if len(axes) == 0:
+            axes = None
+        elif len(axes) == 1 and (isinstance(axes[0], (tuple, list)) or
+                                 axes[0] is None):
+            axes = axes[0]
+        return dezero.functions.transpose(self, axes)
+
     @property
     def T(self):
         return dezero.functions.transpose(self)
@@ -321,7 +334,5 @@ def setup_variable():
     Variable.sum = dezero.functions.sum
     Variable.matmaul = dezero.functions.matmul
     Variable.dot = dezero.functions.matmul
-    Variable.transpose = dezero.functions.transpose
-    Variable.reshape = dezero.functions.reshape
     Variable.max = dezero.functions.max
     Variable.min = dezero.functions.min
