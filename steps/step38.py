a63abf0ea5190a2f4e0cf465d525aeb59fdbc5cf
Koki Saitoh <koki0702@gmail.com>
2019-11-25 11:35:41 +0900

    Add step38.py

---
+++ steps/step38.py
@@ -0,0 +1,18 @@
+import numpy as np
+from dezero import Variable
+import dezero.functions as F
+
+x = Variable(np.array([[0, 1, 2], [3, 4, 5]]))
+y = F.reshape(x, (6,))
+# もしくは y = x.reshape(6)
+y.backward(retain_grad=True)
+print(y.grad)
+
+
+x = Variable(np.array([[1, 2, 3], [4, 5, 6]]))
+y = F.transpose(x)
+# もしくは y = x.T
+y.backward()
+print(x.grad)
+
+
