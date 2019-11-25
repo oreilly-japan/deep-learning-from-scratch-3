e9579943b71c7e05442c8687116e7a865061ac3b
Koki Saitoh <koki0702@gmail.com>
2019-11-25 13:30:02 +0900

    Remove use of sum()

--- steps/step40.py
+++ steps/step40.py
@@ -1,12 +1,9 @@
 import numpy as np
 from dezero import Variable
-import dezero.functions as F
 
 x0 = Variable(np.array([1, 2, 3]))
 x1 = Variable(np.array([10]))
-z = x0 + x1
-# y = z.sum()
-y = F.sum(z)
+y = x0 + x1
 print(y)
 
 y.backward()
