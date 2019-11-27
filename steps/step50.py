1d1309ee030f99924103fb6890bcba083002b718
Koki Saitoh <koki0702@gmail.com>
2019-11-27 09:47:03 +0900

    Use break

--- steps/step50.py
+++ steps/step50.py
@@ -2,7 +2,7 @@ from dezero.datasets import get_mnist
 from dezero.dataset import DatasetLoader
 
 batch_size = 10
-max_epoch = 10
+max_epoch = 1
 
 train, test = get_mnist()
 train_loader = DatasetLoader(train, batch_size)
@@ -11,7 +11,8 @@ test_loader = DatasetLoader(test, batch_size, shuffle=False)
 for epoch in range(max_epoch):
     for x, t in train_loader:
         print(x, t)  # x, tは訓練データ
-
+        break
     # エポックの終わりにテストデータを取り出す
     for x, t in test_loader:
         print(x, t)  # x, tはテストデータ
+        break
