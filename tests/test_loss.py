2f0a851159cb5755f04080b15a09f52d0e23b1e5
Koki Saitoh <koki0702@gmail.com>
2019-11-27 09:46:41 +0900

    Add tests for MSE

---
+++ tests/test_loss.py
@@ -0,0 +1,51 @@
+import unittest
+import numpy as np
+from dezero import Variable
+import dezero.functions as F
+from dezero.utils import check_backward
+
+
+class TestMSE_simple(unittest.TestCase):
+    def test_forward1(self):
+        x0 = np.array([0.0, 1.0, 2.0])
+        x1 = np.array([0.0, 1.0, 2.0])
+        expected = ((x0 - x1)**2).sum() / x0.size
+        y = F.mean_squared_error_simple(x0, x1)
+        self.assertTrue(np.allclose(y.data, expected))
+
+    def test_backward1(self):
+        x0 = np.random.rand(10)
+        x1 = np.random.rand(10)
+
+        f = lambda x0: F.mean_squared_error_simple(x0, x1)
+        self.assertTrue(check_backward(f, x0))
+
+    def test_backward2(self):
+        x0 = np.random.rand(100)
+        x1 = np.random.rand(100)
+
+        f = lambda x0: F.mean_squared_error_simple(x0, x1)
+        self.assertTrue(check_backward(f, x0))
+
+
+class TestMSE_simple(unittest.TestCase):
+    def test_forward1(self):
+        x0 = np.array([0.0, 1.0, 2.0])
+        x1 = np.array([0.0, 1.0, 2.0])
+        expected = ((x0 - x1)**2).sum() / x0.size
+        y = F.mean_squared_error(x0, x1)
+        self.assertTrue(np.allclose(y.data, expected))
+
+    def test_backward1(self):
+        x0 = np.random.rand(10)
+        x1 = np.random.rand(10)
+
+        f = lambda x0: F.mean_squared_error(x0, x1)
+        self.assertTrue(check_backward(f, x0))
+
+    def test_backward2(self):
+        x0 = np.random.rand(100)
+        x1 = np.random.rand(100)
+
+        f = lambda x0: F.mean_squared_error(x0, x1)
+        self.assertTrue(check_backward(f, x0))
