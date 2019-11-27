12d739d8a3c1ffe4cc64b845fe67dea3991108d9
Koki Saitoh <koki0702@gmail.com>
2019-11-27 09:46:27 +0900

    Add tests for softmax_simple

--- tests/test_softmax.py
+++ tests/test_softmax.py
@@ -6,6 +6,49 @@ from dezero.utils import check_backward
 import chainer.functions as CF
 
 
+class TestSoftmaxSimple(unittest.TestCase):
+    def test_forward1(self):
+        x = np.array([[0, 1, 2], [0, 2, 4]], np.float32)
+        y2 = CF.softmax(x, axis=1)
+        y = F.softmax_simple(Variable(x))
+        res = np.allclose(y.data, y2.data)
+        print(y, y2)
+        self.assertTrue(res)
+
+    def test_forward2(self):
+        np.random.seed(0)
+        x = np.random.rand(10, 10).astype('f')
+        y2 = CF.softmax(x, axis=1)
+        y = F.softmax_simple(Variable(x))
+        res = np.allclose(y.data, y2.data)
+        self.assertTrue(res)
+
+    def test_forward3(self):
+        np.random.seed(0)
+        x = np.random.rand(10, 10, 10).astype('f')
+        y2 = CF.softmax(x, axis=1)
+        y = F.softmax_simple(Variable(x))
+        res = np.allclose(y.data, y2.data)
+        self.assertTrue(res)
+
+    def test_backward1(self):
+        x_data = np.array([[0, 1, 2], [0, 2, 4]])
+        f = lambda x: F.softmax_simple(x, axis=1)
+        self.assertTrue(check_backward(f, x_data))
+
+    def test_backward2(self):
+        np.random.seed(0)
+        x_data = np.random.rand(10, 10)
+        f = lambda x: F.softmax_simple(x, axis=1)
+        self.assertTrue(check_backward(f, x_data))
+
+    def test_backward3(self):
+        np.random.seed(0)
+        x_data = np.random.rand(10, 10, 10)
+        f = lambda x: F.softmax_simple(x, axis=1)
+        self.assertTrue(check_backward(f, x_data))
+
+
 class TestSoftmax(unittest.TestCase):
     def test_forward1(self):
         x = np.array([[0, 1, 2], [0, 2, 4]], np.float32)
