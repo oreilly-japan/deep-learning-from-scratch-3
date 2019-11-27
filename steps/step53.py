3fc6bcc3dfc67457191aa1356c641e716073a9c8
Koki Saitoh <koki0702@gmail.com>
2019-11-27 17:04:23 +0900

    Use MLP

--- steps/step53.py
+++ steps/step53.py
@@ -2,7 +2,7 @@ import dezero
 import dezero.functions as F
 from dezero import optimizers
 from dezero.dataset import DatasetLoader
-from dezero.models import TwoLayerNet
+from dezero.models import MLP
 
 
 max_epoch = 3
@@ -10,7 +10,7 @@ batch_size = 100
 
 train_set, test_set = dezero.datasets.get_mnist()
 train_loader = DatasetLoader(train_set, batch_size)
-model = TwoLayerNet(784, 1000, 10)
+model = MLP((784, 1000, 10))
 optimizer = optimizers.SGD().setup(model)
 
 # パラーメタの読み込み
