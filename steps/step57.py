6a63134cb43324c8630856dadbbb08170bb06805
Koki Saitoh <koki0702@gmail.com>
2019-12-09 09:12:14 +0900

    Add step code

--- steps/step57.py
+++ steps/step57.py
@@ -1,1 +1,27 @@
-# WIP
+import numpy as np
+from dezero import Variable
+import dezero.functions as F
+
+
+# im2col
+x1 = np.random.rand(1, 3, 7, 7)
+col1 = F.im2col(x1, kernel_size=5, stride=1, pad=0, to_matrix=True)
+print(col1.shape)  # (9, 75)
+
+x2 = np.random.rand(10, 3, 7, 7)  # 10個のデータ
+kernel_size = (5, 5)
+stride = (1, 1)
+pad = (0, 0)
+col2 = F.im2col(x2, kernel_size, stride, pad, to_matrix=True)
+print(col2.shape)  # (90, 75)
+
+
+# conv2d
+N, C, H, W = 1, 5, 15, 15
+OC, (KH, KW) = 8, (3, 3)
+x = Variable(np.random.randn(N, C, H, W))
+W = np.random.randn(OC, C, KH, KW)
+y = F.conv2d_simple(x, W, b=None, stride=1, pad=1)
+y.backward()
+print(y.shape)  # (1, 8, 15, 15)
+print(x.grad.shape)  # (1, 5, 15, 15)
