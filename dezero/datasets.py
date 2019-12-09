8c5248d0d00058cf923d6ebfc6cca90d3ff739f3
Koki Saitoh <koki0702@gmail.com>
2019-12-09 13:58:29 +0900

    Use utils's get_file()

--- dezero/datasets.py
+++ dezero/datasets.py
@@ -115,12 +115,20 @@ def get_mnist(ndim=1, scale=1.0, dtype=np.float32):
     return m.get_mnist(ndim, scale, dtype)
 
 
-def load_shakespear():
+def get_imagenet_labels():
+    url = 'https://gist.githubusercontent.com/yrevar/942d3a0ac09ec9e5eb3a/raw/238f720ff059c1f82f368259d1ca4ffa5dd8f9f5/imagenet1000_clsidx_to_labels.txt'
+    path = get_file(url)
+    with open(path, 'r') as f:
+        labels = eval(f.read())
+    return labels
+
+
+def get_shakespear():
     url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
     file_name = 'shakespear.txt'
-    get_file(url, file_name)
+    path = get_file(url, file_name)
 
-    with open(os.path.join(cache_dir, file_name), 'r') as f:
+    with open(path, 'r') as f:
         data = f.read()
     chars = list(data)
 
