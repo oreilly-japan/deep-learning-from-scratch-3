9682814b79a5809d4d1dde821eaa31110a48ec89
Koki Saitoh <koki0702@gmail.com>
2019-12-11 21:36:15 +0900

    Add BatchNorm

--- dezero/functions.py
+++ dezero/functions.py
@@ -469,8 +469,72 @@ def dropout(x, dropout_ratio=0.5):
         return x
 
 
-def batch_nrom(x):
-    pass
+class BatchNorm(Function):
+    def __init__(self, mean, var, decay, eps):
+        self.avg_mean = mean
+        self.avg_var = var
+        self.decay = decay
+        self.eps = eps
+        self.inv_std = None
+
+    def forward(self, x, gamma, beta):
+        assert x.ndim == 2 or x.ndim == 4
+
+        x_ndim = x.ndim
+        if x_ndim == 4:
+            N, C, H, W = x.shape
+            # (N, C, H, W) -> (N*H*W, C)
+            x = x.transpose(0, 2, 3, 1).reshape(-1, C)
+
+        xp = cuda.get_array_module(x)
+
+        if dezero.Config.train:
+            mean = x.mean(axis=0)
+            var = x.var(axis=0)
+            inv_std = 1 / xp.sqrt(var + self.eps)
+            xc = (x - mean) * inv_std
+            self.avg_mean *= self.decay
+            self.avg_mean += (1 - self.decay) * mean
+            self.avg_var *= self.decay
+            self.avg_var += (1 - self.decay) * var
+            self.inv_std = inv_std
+        else:
+            inv_std = 1 / xp.sqrt(self.avg_var + self.eps)
+            xc = (x - self.avg_mean) * inv_std
+        y = gamma * xc + beta
+
+        if x_ndim == 4:
+            # (N*H*W, C) -> (N, C, H, W)
+            y = y.reshape(N, H, W, C).transpose(0, 3, 1, 2)
+        return y
+
+    def backward(self, gy):
+        gy_ndim = gy.ndim
+        if gy_ndim == 4:
+            N, C, H, W = gy.shape
+            gy = gy.transpose(0, 2, 3, 1).reshape(-1, C)
+
+        x, gamma, beta = self.inputs
+        batch_size = len(gy)
+
+        if x.ndim == 4:
+            N, C, H, W = x.shape
+            x = x.transpose(0, 2, 3, 1).reshape(-1, C)
+        mean = x.sum(axis=0) / batch_size
+        xc = (x - mean) * self.inv_std
+
+        gbeta = sum(gy, axis=0)
+        ggamma = sum(xc * gy, axis=0)
+        gx = gy - gbeta / batch_size - xc * ggamma / batch_size
+        gx *= gamma * self.inv_std
+
+        if gy_ndim == 4:
+            gx = gx.reshape(N, H, W, C).transpose(0, 3, 1, 2)
+        return gx, ggamma, gbeta
+
+
+def batch_nrom(x, gamma, beta, mean, var, decay=0.9, eps=2e-5):
+    return BatchNorm(mean, var, decay, eps)(x, gamma, beta)
 
 
 # =============================================================================
