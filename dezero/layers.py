c9d135143dcdbfd8c23cdfc078649f4ec90bee17
Koki Saitoh <koki0702@gmail.com>
2019-12-11 21:36:39 +0900

    Add BatchNorm layer

--- dezero/layers.py
+++ dezero/layers.py
@@ -1,4 +1,5 @@
 import numpy as np
+import dezero
 import dezero.functions as F
 from dezero import cuda
 from dezero.core import Parameter
@@ -233,3 +234,42 @@ class LSTM(Layer):
 
         self.h, self.c = h, c
         return h
+
+
+class BatchNorm(Layer):
+    def __init__(self):
+        super().__init__()
+        # BatchNorm's avg_mean and avg_var are not trainable but they have to
+        # be saved to a file. So the two values are Parameter object.
+        self._avg_mean = Parameter(None, name='avg_mean')
+        self._avg_var = Parameter(None, name='avg_var')
+        self.gamma = Parameter(None, name='gamma')
+        self.beta = Parameter(None, name='beta')
+
+    @property
+    def avg_mean(self):
+        return self._avg_mean.data
+
+    @property
+    def avg_var(self):
+        return self._avg_var.data
+
+    def _initialize_params(self, x):
+        xp = cuda.get_array_module(x)
+        D = x.shape[1]
+        if self._avg_mean.data is None:
+            self._avg_mean.data = xp.zeros(D, dtype=x.dtype)
+        if self._avg_var.data is None:
+            self._avg_var.data = xp.ones(D, dtype=x.dtype)
+        if self.gamma.data is None:
+            self.gamma.data = xp.ones(D, dtype=x.dtype)
+        if self.beta is None:
+            self.beta.data = xp.zeros(D, dtype=x.dtype)
+
+    def __call__(self, x):
+        if self.avg_mean is None:
+            self._initialize_params(x)
+        return F.batch_nrom(x, self.gamma, self.beta, self.avg_mean,
+                            self.avg_var)
+
+
