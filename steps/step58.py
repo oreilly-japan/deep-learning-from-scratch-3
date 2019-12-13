cb0972a70434a33a3d2f9dfbb796399921d380c2
Koki Saitoh <koki0702@gmail.com>
2019-12-13 09:38:02 +0900

    Minor Update

--- steps/step58.py
+++ steps/step58.py
@@ -5,19 +5,16 @@ from dezero.models import VGG16
 from dezero.dataset import preprocess_vgg
 from dezero.datasets import get_imagenet_labels
 
-
 url = 'https://github.com/oreilly-japan/deep-learning-from-scratch-3/raw/images/zebra.jpg'
 img_path = dezero.utils.get_file(url)
 img = Image.open(img_path)
 x = preprocess_vgg(img)
 x = x[np.newaxis]
 
-labels = get_imagenet_labels()
-
 model = VGG16(pretrained=True)
 with dezero.test_mode():
     y = model(x)
-
 predict_id = np.argmax(y.data)
+model.plot(x, to_file='vgg.pdf')
+labels = get_imagenet_labels()
 print(labels[predict_id])
-img.show()
