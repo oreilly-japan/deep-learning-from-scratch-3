d7f012e432aa2c26da4873d604cd2b98bb5e9e9f
Koki Saitoh <koki0702@gmail.com>
2019-12-17 09:06:45 +0900

    Add SeqDataLoader

--- dezero/dataset.py
+++ dezero/dataset.py
@@ -1,3 +1,4 @@
+from operator import itemgetter
 import math
 pil_available = True
 try:
@@ -92,20 +93,22 @@ class DatasetLoader:
         self.gpu = True
 
 
-class SequentialDataLoader(DatasetLoader):
+class SeqDataLoader(DatasetLoader):
     def __init__(self, dataset, batch_size, preprocess=None, gpu=False):
         super().__init__(dataset=dataset, batch_size=batch_size, shuffle=False,
                          preprocess=preprocess, gpu=gpu)
-        self.dataset = np.array(self.dataset)
 
     def _get_batch(self):
         jump = self.data_size // self.batch_size
         offsets = [(i * jump + self.iteration) % self.data_size for i in
                    range(self.batch_size)]
-        batch = self.dataset[offsets]
+        batch = itemgetter(*offsets)(self.dataset)
         return batch
 
 
+# =============================================================================
+# Preprocess function
+# =============================================================================
 def preprocess_vgg(image, size=(224, 224), dtype=np.float32):
     """VGGで使用する画像に対して前処理を施しndarrayへと変換する
     VGGのpre-trainedモデルでは、下記の前処理を行う
