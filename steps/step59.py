57bbacd803be233795bffaeace9ee93b5e138f3e
Koki Saitoh <koki0702@gmail.com>
2019-12-17 09:08:45 +0900

    Add rnn/lstm code

--- steps/step59.py
+++ steps/step59.py
@@ -1,1 +1,70 @@
-# WIP
+import numpy as np
+import matplotlib.pyplot as plt
+import dezero
+from dezero import Model
+import dezero.functions as F
+import dezero.layers as L
+
+# Hyperparameters
+max_epoch = 20
+hidden_size = 100
+bptt_length = 30
+
+
+train_set, test_set = dezero.datasets.get_sin()
+seqlen = len(train_set)
+
+
+class SimpleRNN(Model):
+    def __init__(self, hidden_size, out_size):
+        super().__init__()
+        self.rnn = L.RNN(hidden_size)
+        self.h2y = L.Linear(out_size)
+
+    def reset_state(self):
+        self.rnn.reset_state()
+
+    def __call__(self, x):
+        h = self.rnn(x)
+        y = self.h2y(h)
+        return y
+
+
+model = SimpleRNN(hidden_size, 1)
+optimizer = dezero.optimizers.Adam().setup(model)
+
+# Start training.
+for epoch in range(max_epoch):
+    model.reset_state()
+    loss, count = 0, 0
+
+    for x, t in train_set:
+        x = x[:, np.newaxis]  # Add axis for batch.
+        y = model(x)
+        loss += F.mean_squared_error(y, t)
+        count += 1
+
+        if count % bptt_length == 0 or count == seqlen:
+            model.cleargrads()
+            loss.backward()
+            loss.unchain_backward()
+            optimizer.update()
+
+    avg_loss = float(loss.data) / count
+    print('| epoch %d | loss %f' % (epoch + 1,avg_loss))
+
+# Plot
+xs = np.cos(np.linspace(0, 4 * np.pi, 1000))
+model.reset_state()
+pred_list = []
+with dezero.no_grad():
+    for x in xs:
+        x = np.array(x).reshape(1, 1)
+        y = model(x)
+        pred_list.append(float(y.data))
+plt.plot(np.arange(len(xs)), xs, label='y=cos(x)')
+plt.plot(np.arange(len(xs)), pred_list, label='predict')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.show()
--- steps/step60.py
+++ steps/step60.py
@@ -1,1 +1,68 @@
-# WIP
+import numpy as np
+import matplotlib.pyplot as plt
+import dezero
+from dezero import Model
+from dezero.dataset import SeqDataLoader
+import dezero.functions as F
+import dezero.layers as L
+
+
+max_epoch = 100
+batch_size = 30
+hidden_size = 100
+bptt_length = 30
+
+train_set, test_set = dezero.datasets.get_sin()
+dataloader = SeqDataLoader(train_set, batch_size=batch_size)
+seqlen = len(train_set)
+
+
+class BetterRNN(Model):
+    def __init__(self, hidden_size, out_size):
+        super().__init__()
+        self.rnn = L.LSTM(hidden_size)
+        self.fc = L.Linear(out_size)
+
+    def reset_state(self):
+        self.rnn.reset_state()
+
+    def __call__(self, x):
+        y = self.rnn(x)
+        y = self.fc(y)
+        return y
+
+model = BetterRNN(hidden_size, 1)
+optimizer = dezero.optimizers.Adam().setup(model)
+
+for epoch in range(max_epoch):
+    model.reset_state()
+    loss, count = 0, 0
+
+    for x, t in dataloader:
+        y = model(x)
+        loss += F.mean_squared_error(y, t)
+        count += 1
+
+        if count % bptt_length == 0 or count == seqlen:
+            model.cleargrads()
+            loss.backward()
+            loss.unchain_backward()
+            optimizer.update()
+    avg_loss = float(loss.data) / count
+    print('| epoch %d | loss %f' % (epoch + 1, avg_loss))
+
+# Plot
+xs = np.cos(np.linspace(0, 4 * np.pi, 1000))
+model.reset_state()
+pred_list = []
+with dezero.no_grad():
+    for x in xs:
+        x = np.array(x).reshape(1, 1)
+        y = model(x)
+        pred_list.append(float(y.data))
+plt.plot(np.arange(len(xs)), xs, label='y=cos(x)')
+plt.plot(np.arange(len(xs)), pred_list, label='predict')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.show()
