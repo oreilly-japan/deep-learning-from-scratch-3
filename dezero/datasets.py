0b963c99ec690f56efc5be393b364915a6a661ff
Koki Saitoh <koki0702@gmail.com>
2019-12-23 07:21:07 +0900

    Add CIFAR10

--- dezero/datasets.py
+++ dezero/datasets.py
@@ -1,11 +1,17 @@
+import os
 import gzip
+import tarfile
+import pickle
 import numpy as np
 import matplotlib.pyplot as plt
-from dezero.utils import get_file
-from dezero.data import Dataset, TupleDataset
+from dezero.utils import get_file, cache_dir
+from dezero.data import Dataset
 from dezero.transforms import Compose, Flatten, ToFloat, Normalize
 
 
+# =============================================================================
+# Toy datasets
+# =============================================================================
 class Spiral(Dataset):
 
     def prepare(self):
@@ -33,6 +39,9 @@ class Spiral(Dataset):
         self.data, self.label = x, t
 
 
+# =============================================================================
+# MNIST-like dataset: MNIST / CIFAR /
+# =============================================================================
 class MNIST(Dataset):
 
     def __init__(self, train=True,
@@ -79,10 +88,144 @@ class MNIST(Dataset):
 
     @staticmethod
     def labels():
-        return {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7',
-                8: '8', 9: '9'}
+        return {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9'}
 
 
+class CIFAR10(Dataset):
+
+    def __init__(self, train=True,
+                 transforms=Compose([ToFloat(), Normalize(mean=0.5, std=0.5)]),
+                 target_transforms=None):
+        super().__init__(train, transforms, target_transforms)
+
+    def prepare(self):
+        url='https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
+        self.data, self.label = load_cache_npz(url, self.train)
+        if self.data is not None:
+            return
+        filepath = get_file(url)
+        if self.train:
+            self.data = np.empty((50000, 3 * 32 * 32))
+            self.label = np.empty((50000), dtype=np.int)
+            for i in range(5):
+                self.data[i * 10000:(i + 1) * 10000] = self._load_data(
+                    filepath, i + 1, 'train')
+                self.label[i * 10000:(i + 1) * 10000] = self._load_label(
+                    filepath, i + 1, 'train')
+        else:
+            self.data = self._load_data(filepath, 5, 'test')
+            self.label = self._load_label(filepath, 5, 'test')
+        self.data = self.data.reshape(-1, 3, 32, 32)
+        save_cache_npz(self.data, self.label, url, self.train)
+
+
+    def _load_data(self, filename, idx, data_type='train'):
+        assert data_type in ['train', 'test']
+        with tarfile.open(filename, 'r:gz') as file:
+            for item in file.getmembers():
+                if ('data_batch_{}'.format(idx) in item.name and data_type == 'train') or ('test_batch' in item.name and data_type == 'test'):
+                    data_dict = pickle.load(file.extractfile(item), encoding='bytes')
+                    data = data_dict[b'data']
+                    return data
+
+    def _load_label(self, filename, idx, data_type='train'):
+        assert data_type in ['train', 'test']
+        with tarfile.open(filename, 'r:gz') as file:
+            for item in file.getmembers():
+                if ('data_batch_{}'.format(idx) in item.name and data_type == 'train') or ('test_batch' in item.name and data_type == 'test'):
+                    data_dict = pickle.load(file.extractfile(item), encoding='bytes')
+                    return np.array(data_dict[b'labels'])
+
+    def show(self, row=10, col=10):
+        H, W = 32, 32
+        img = np.zeros((H*row, W*col, 3))
+        for r in range(row):
+            for c in range(col):
+                img[r*H:(r+1)*H, c*W:(c+1)*W] = self.data[np.random.randint(0, len(self.data)-1)].reshape(3,H,W).transpose(1,2,0)/255
+        plt.imshow(img, interpolation='nearest')
+        plt.axis('off')
+        plt.show()
+
+    @staticmethod
+    def labels():
+        return {0: 'ariplane', 1: 'automobile', 2: 'bird', 3: 'cat', 4: 'deer', 5: 'dog', 6: 'frog', 7: 'horse', 8: 'ship', 9: 'truck'}
+
+
+class CIFAR100(CIFAR10):
+
+    def __init__(self, train=True,
+                 transforms=Compose([ToFloat(), Normalize(mean=0.5, std=0.5)]),
+                 target_transforms=None,
+                 label_type='fine'):
+        assert label_type in ['fine', 'coarse']
+        self.label_type = label_type
+        super().__init__(train, transforms, target_transforms)
+
+    def prepare(self):
+        url = 'https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz'
+        self.data, self.label = load_cache_npz(url, self.train)
+        if self.data is not None:
+            return
+
+        filepath = get_file(url)
+        if self.train:
+            self.data = self._load_data(filepath, 'train')
+            self.label = self._load_label(filepath, 'train')
+        else:
+            self.data = self._load_data(filepath, 'test')
+            self.label = self._load_label(filepath, 'test')
+        self.data = self.data.reshape(-1, 3, 32, 32)
+        save_cache_npz(self.data, self.label, url, self.train)
+
+    def _load_data(self, filename, data_type='train'):
+        with tarfile.open(filename, 'r:gz') as file:
+            for item in file.getmembers():
+                if data_type in item.name:
+                    data_dict = pickle.load(file.extractfile(item), encoding='bytes')
+                    data = data_dict[b'data']
+                    return data
+
+    def _load_label(self, filename, data_type='train'):
+        assert data_type in ['train', 'test']
+        with tarfile.open(filename, 'r:gz') as file:
+            for item in file.getmembers():
+                if data_type in item.name:
+                    data_dict = pickle.load(file.extractfile(item), encoding='bytes')
+                    if self.label_type == 'fine':
+                        return np.array(data_dict[b'fine_labels'])
+                    elif self.label_type == 'coarse':
+                        return np.array(data_dict[b'coarse_labels'])
+
+    @staticmethod
+    def labels(label_type='fine'):
+        coarse_labels = dict(enumerate(['aquatic mammals','fish','flowers','food containers','fruit and vegetables','household electrical device','household furniture','insects','large carnivores','large man-made outdoor things','large natural outdoor scenes','large omnivores and herbivores','medium-sized mammals','non-insect invertebrates','people','reptiles','small mammals','trees','vehicles 1','vehicles 2']))
+        fine_labels = []
+        return fine_labels if label_type is 'fine' else coarse_labels
+
+
+
+
+
+    # =============================================================================
+# Big datasets
+# =============================================================================
+class ImageNet(Dataset):
+
+    def __init__(self):
+        NotImplemented
+
+    @staticmethod
+    def labels():
+        url = 'https://gist.githubusercontent.com/yrevar/942d3a0ac09ec9e5eb3a/raw/238f720ff059c1f82f368259d1ca4ffa5dd8f9f5/imagenet1000_clsidx_to_labels.txt'
+        path = get_file(url)
+        with open(path, 'r') as f:
+            labels = eval(f.read())
+        return labels
+
+
+# =============================================================================
+# Sequential datasets: SinCurve, Shapekspare
+# =============================================================================
 class SinCurve(Dataset):
 
     def prepare(self):
@@ -101,36 +244,58 @@ class SinCurve(Dataset):
         self.label = y[1:][:, np.newaxis]
 
 
-class ImageNet(Dataset):
+class Shakespear(Dataset):
 
-    def __init__(self):
-        NotImplemented
-
-    @staticmethod
-    def labels():
-        url = 'https://gist.githubusercontent.com/yrevar/942d3a0ac09ec9e5eb3a/raw/238f720ff059c1f82f368259d1ca4ffa5dd8f9f5/imagenet1000_clsidx_to_labels.txt'
-        path = get_file(url)
+    def prepare(self):
+        url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
+        file_name = 'shakespear.txt'
+        path = get_file(url, file_name)
         with open(path, 'r') as f:
-            labels = eval(f.read())
-        return labels
+            data = f.read()
+        chars = list(data)
+
+        char_to_id = {}
+        id_to_char = {}
+        for word in data:
+            if word not in char_to_id:
+                new_id = len(char_to_id)
+                char_to_id[word] = new_id
+                id_to_char[new_id] = word
+
+        indices = np.array([char_to_id[c] for c in chars])
+        self.data = indices[:-1]
+        self.label = indices[1:]
+        self.char_to_id = char_to_id
+        self.id_to_char = id_to_char
 
 
-def get_shakespear():
-    url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
-    file_name = 'shakespear.txt'
-    path = get_file(url, file_name)
+# =============================================================================
+# Utils
+# =============================================================================
+def load_cache_npz(filename, train=False):
+    filename = filename[filename.rfind('/') + 1:]
+    prefix = '.train.npz' if train else '.test.npz'
+    filepath = os.path.join(cache_dir, filename + prefix)
+    if not os.path.exists(filepath):
+        return None, None
 
-    with open(path, 'r') as f:
-        data = f.read()
-    chars = list(data)
+    loaded = np.load(filepath)
+    return loaded['data'], loaded['label']
 
-    char_to_id = {}
-    id_to_char = {}
-    for word in data:
-        if word not in char_to_id:
-            new_id = len(char_to_id)
-            char_to_id[word] = new_id
-            id_to_char[new_id] = word
+def save_cache_npz(data, label, filename, train=False):
+    filename = filename[filename.rfind('/') + 1:]
+    prefix = '.train.npz' if train else '.test.npz'
+    filepath = os.path.join(cache_dir, filename + prefix)
 
-    indices = np.array([char_to_id[c] for c in chars])
-    return indices, char_to_id, id_to_char
+    if os.path.exists(filepath):
+        return
+
+    print("Saving: " + filename + prefix)
+    try:
+        np.savez_compressed(filepath, data=data, label=label)
+    except (Exception, KeyboardInterrupt) as e:
+        if os.path.exists(filepath):
+            os.remove(filepath)
+        raise
+    print(" Done")
+    return filepath
