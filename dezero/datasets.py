a910de0602a4e097ed5b7a3e0dde34138d384edc
Koki Saitoh <koki0702@gmail.com>
2019-12-24 18:09:28 +0900

    Add get_spiral()

--- dezero/datasets.py
+++ dezero/datasets.py
@@ -40,31 +40,34 @@ class Dataset:
 # =============================================================================
 # Toy datasets
 # =============================================================================
+def get_spiral(train=True):
+    seed = 1984 if train else 2020
+    np.random.seed(seed=seed)
+
+    num_data, num_class, input_dim = 100, 3, 2
+    data_size = num_class * num_data
+    x = np.zeros((data_size, input_dim), dtype=np.float32)
+    t = np.zeros(data_size, dtype=np.int)
+
+    for j in range(num_class):
+        for i in range(num_data):
+            rate = i / num_data
+            radius = 1.0 * rate
+            theta = j * 4.0 + 4.0 * rate + np.random.randn() * 0.2
+            ix = num_data * j + i
+            x[ix] = np.array([radius * np.sin(theta),
+                              radius * np.cos(theta)]).flatten()
+            t[ix] = j
+    # Shuffle
+    indices = np.random.permutation(num_data * num_class)
+    x = x[indices]
+    t = t[indices]
+    return x, t
+
+
 class Spiral(Dataset):
-
     def prepare(self):
-        seed = 1984 if self.train else 2020
-        np.random.seed(seed=seed)
-
-        num_data, num_class, input_dim = 100, 3, 2
-        data_size = num_class * num_data
-        x = np.zeros((data_size, input_dim), dtype=np.float32)
-        t = np.zeros(data_size, dtype=np.int)
-
-        for j in range(num_class):
-            for i in range(num_data):
-                rate = i / num_data
-                radius = 1.0 * rate
-                theta = j * 4.0 + 4.0 * rate + np.random.randn() * 0.2
-                ix = num_data * j + i
-                x[ix] = np.array([radius * np.sin(theta),
-                                  radius * np.cos(theta)]).flatten()
-                t[ix] = j
-        # Shuffle
-        indices = np.random.permutation(num_data * num_class)
-        x = x[indices]
-        t = t[indices]
-        self.data, self.label = x, t
+        self.data, self.label = get_spiral(self.train)
 
 
 # =============================================================================
