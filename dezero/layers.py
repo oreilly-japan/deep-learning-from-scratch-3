feec446c72b6c61be4fcd67f0911d27486c503d1
Koki Saitoh <koki0702@gmail.com>
2020-01-14 15:05:24 +0900

    Add deconv2d layer

--- dezero/layers.py
+++ dezero/layers.py
@@ -76,7 +76,7 @@ class Layer:
 
 
 # =============================================================================
-# Linear / Conv2d
+# Linear / Conv2d / Deconv2d
 # =============================================================================
 class Linear(Layer):
     def __init__(self, out_size, nobias=False, dtype=np.float32, in_size=None):
@@ -144,8 +144,8 @@ class Conv2d(Layer):
     def _init_W(self, xp=np):
         C, OC = self.in_channels, self.out_channels
         KH, KW = pair(self.kernel_size)
-        W_data = xp.random.randn(OC, C, KH, KW).astype(self.dtype) * np.sqrt(
-            1 / C * KH * KW)
+        scale = np.sqrt(1 / (C * KH * KW))
+        W_data = xp.random.randn(OC, C, KH, KW).astype(self.dtype) * scale
         self.W.data = W_data
 
     def __call__(self, x):
@@ -155,6 +155,55 @@ class Conv2d(Layer):
             self._init_W(xp)
 
         y = F.conv2d(x, self.W, self.b, self.stride, self.pad)
+        return y
+
+
+class Deconv2d(Layer):
+    def __init__(self, out_channels, kernel_size, stride=1,
+                 pad=0, nobias=False, dtype=np.float32, in_channels=None):
+        """Two-dimensional deconvolutional (transposed convolution)layer.
+
+        Args:
+            out_channels (int): Number of channels of output arrays.
+            kernel_size (int or (int, int)): Size of filters.
+            stride (int or (int, int)): Stride of filter applications.
+            pad (int or (int, int)): Spatial padding width for input arrays.
+            nobias (bool): If `True`, then this function does not use the bias.
+            in_channels (int or None): Number of channels of input arrays. If
+            `None`, parameter initialization will be deferred until the first
+            forward data pass at which time the size will be determined.
+        """
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.pad = pad
+        self.dtype = dtype
+
+        self.W = Parameter(None, name='W')
+        if in_channels is not None:
+            self._init_W()
+
+        if nobias:
+            self.b = None
+        else:
+            self.b = Parameter(np.zeros(out_channels, dtype=dtype), name='b')
+
+    def _init_W(self, xp=np):
+        C, OC = self.in_channels, self.out_channels
+        KH, KW = pair(self.kernel_size)
+        scale = np.sqrt(1 / (C * KH * KW))
+        W_data = xp.random.randn(C, OC, KH, KW).astype(self.dtype) * scale
+        self.W.data = W_data
+
+    def __call__(self, x):
+        if self.W.data is None:
+            self.in_channels = x.shape[1]
+            xp = cuda.get_array_module(x)
+            self._init_W(xp)
+
+        y = F.deconv2d(x, self.W, self.b, self.stride, self.pad)
         return y
 
 
