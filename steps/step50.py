4b0e532cc731ad3d4ecf8318b016527ffb58ba9e
Koki Saitoh <koki0702@gmail.com>
2020-01-14 15:14:29 +0900

    Add training code

--- steps/step50.py
+++ steps/step50.py
@@ -1,20 +1,50 @@
 import os, sys; sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from dezero.datasets import Spiral
+import dezero
+import dezero.functions as F
+from dezero import optimizers
 from dezero import DataLoader
+from dezero.models import MLP
 
-batch_size = 10
-max_epoch = 1
 
-train_set = Spiral(train=True)
-test_set = Spiral(train=False)
+max_epoch = 300
+batch_size = 30
+hidden_size = 10
+lr = 1.0
+
+train_set = dezero.datasets.Spiral(train=True)
+test_set = dezero.datasets.Spiral(train=False)
 train_loader = DataLoader(train_set, batch_size)
 test_loader = DataLoader(test_set, batch_size, shuffle=False)
 
-for epoch in range(max_epoch):
-    for x, t in train_loader:
-        print(x.shape, t.shape)
-        break
+model = MLP((hidden_size, 10))
+optimizer = optimizers.SGD(lr).setup(model)
 
-    for x, t in test_loader:
-        print(x.shape, t.shape)
-        break
+for epoch in range(max_epoch):
+    sum_loss, sum_acc = 0, 0
+
+    for x, t in train_loader:
+        y = model(x)
+        loss = F.softmax_cross_entropy(y, t)
+        acc = F.accuracy(y, t)
+        model.cleargrads()
+        loss.backward()
+        optimizer.update()
+
+        sum_loss += float(loss.data) * len(t)
+        sum_acc += float(acc.data) * len(t)
+
+    print('epoch: {}'.format(epoch+1))
+    print('train loss: {:.4f}, accuracy: {:.4f}'.format(
+        sum_loss / len(train_set), sum_acc / len(train_set)))
+
+    sum_loss, sum_acc = 0, 0
+    with dezero.no_grad():
+        for x, t in test_loader:
+            y = model(x)
+            loss = F.softmax_cross_entropy(y, t)
+            acc = F.accuracy(y, t)
+            sum_loss += float(loss.data) * len(t)
+            sum_acc += float(acc.data) * len(t)
+
+    print('test loss: {:.4f}, accuracy: {:.4f}'.format(
+        sum_loss / len(test_set), sum_acc / len(test_set)))
