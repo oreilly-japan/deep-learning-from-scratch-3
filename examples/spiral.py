e2554b2f2858daf50b60b8f39090dc734fecbf2e
Koki Saitoh <koki0702@gmail.com>
2020-01-22 15:51:11 +0900

    Fix linear's args

--- examples/spiral.py
+++ examples/spiral.py
@@ -8,28 +8,30 @@ import dezero.layers as L
 from dezero import DataLoader
 
 
-max_epoch = 300
+max_epoch = 100
 batch_size = 30
 hidden_size = 10
 lr = 1.0
 
-train_set, test_set = dezero.datasets.get_spiral()
+train_set = dezero.datasets.Spiral(train=True)
+test_set = dezero.datasets.Spiral(train=False)
 train_loader = DataLoader(train_set, batch_size)
-
+test_loader = DataLoader(test_set, batch_size, shuffle=False)
 
 class TwoLayerNet(Model):
-    def __init__(self, in_size, hidden_size, out_size):
+    def __init__(self, hidden_size, out_size):
         super().__init__()
-        self.l1 = L.Linear(in_size, hidden_size)
-        self.l2 = L.Linear(hidden_size, out_size)
+        self.l1 = L.Linear(hidden_size)
+        self.l2 = L.Linear(out_size)
+        self.bn1 = L.BatchNorm()
 
     def __call__(self, x):
-        y = F.sigmoid(self.l1(x))
+        y = F.sigmoid(self.bn1(self.l1(x)))
         y = self.l2(y)
         return y
 
 
-model = TwoLayerNet(2, hidden_size, 3)
+model = TwoLayerNet(hidden_size, 3)
 optimizer = optimizers.SGD(lr).setup(model)
 
 for epoch in range(max_epoch):
@@ -51,7 +53,7 @@ y_min, y_max = x[:, 1].min() - .1, x[:, 1].max() + .1
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
 X = np.c_[xx.ravel(), yy.ravel()]
 
-with dezero.no_grad():
+with dezero.test_mode():
     score = model(X)
 predict_cls = np.argmax(score.data, axis=1)
 Z = predict_cls.reshape(xx.shape)
