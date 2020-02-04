161cf74a994b0093c6456dfb5716e4d22fd30bd2
Koki Saitoh <koki0702@gmail.com>
2020-02-04 16:51:46 +0900

    Use len(diff)

--- dezero/functions.py
+++ dezero/functions.py
@@ -404,21 +404,21 @@ def leaky_relu(x, slope=0.2):
 def mean_squared_error_simple(x0, x1):
     x0, x1 = as_variable(x0), as_variable(x1)
     diff = x0 - x1
-    y = sum(diff ** 2) / diff.size
+    y = sum(diff ** 2) / len(diff)
     return y
 
 
 class MeanSquaredError(Function):
     def forward(self, x0, x1):
         diff = x0 - x1
-        y = (diff ** 2).sum() / diff.size
+        y = (diff ** 2).sum() / len(diff)
         return y
 
     def backward(self, gy):
         x0, x1 = self.inputs
         diff = x0 - x1
         gy = broadcast_to(gy, diff.shape)
-        gx0 = gy * diff * (2. / diff.size)
+        gx0 = gy * diff * (2. / len(diff))
         gx1 = -gx0
         return gx0, gx1
 
