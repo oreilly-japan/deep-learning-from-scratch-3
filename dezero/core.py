7bd03c2c388590db64a1a0eed9d00da854d7c5c4
Koki Saitoh <koki0702@gmail.com>
2020-06-01 13:47:50 +0900

    Modifying #13

--- dezero/core.py
+++ dezero/core.py
@@ -195,7 +195,7 @@ class Function:
             for output in outputs:
                 output.set_creator(self)
             self.inputs = inputs
-            self.outputs = [weakref.ref(y) for y in outputs]
+            self.outputs = [weakref.ref(output) for output in outputs]
 
         return outputs if len(outputs) > 1 else outputs[0]
 
